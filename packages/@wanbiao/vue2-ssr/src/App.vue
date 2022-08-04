202d3ddb2f2ff1dd269d333eb2f60ea0ef82ea74
谢树宏 <384180258@qq.com>
2022-08-04 17:54:22 +0800

    :tada: 初始化ssr项目

---
+++ packages/@wanbiao/vue2-ssr/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
